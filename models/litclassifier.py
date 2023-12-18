3be78b9c65e774da1db2a609c947de0ec8be82cc
muhd-umer <thisismumer@gmail.com>
2023-12-18 13:50:19 +0500

    Add SAM optimizer and update training loop

--- models/litclassifier.py
+++ models/litclassifier.py
@@ -15,12 +15,15 @@ import torch.optim.lr_scheduler as lr_scheduler
 import torchmetrics
 from torch import nn, optim
 
+from .sam import SAM
+
 
 class ImageClassifier(pl.LightningModule):
     def __init__(self, model: nn.Module, cfg: dict):
         super().__init__()
         self.model = model
         self.cfg = cfg
+        self.automatic_optimization = False
         self.loss = nn.CrossEntropyLoss()
 
     def forward(self, x: torch.Tensor):
@@ -28,9 +31,20 @@ class ImageClassifier(pl.LightningModule):
 
     def training_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int):
         x, y = batch
-        y_hat = self(x)
-        loss = self.loss(y_hat, y)
+        optimizer = self.optimizers()
+
+        def closure():
+            loss = self.loss(self(x), y)
+            loss.backward()
+            return loss
+
+        loss = self.loss(self(x), y)
+        loss.backward()
+        optimizer.step(closure)
+        optimizer.zero_grad()
+
         self.log("train_loss", loss, prog_bar=True)
+
         return loss
 
     def validation_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int):
@@ -60,8 +74,18 @@ class ImageClassifier(pl.LightningModule):
         self.log("test_acc", acc, prog_bar=True, sync_dist=True)
 
     def configure_optimizers(self):
-        optimizer = optim.AdamW(self.parameters(), lr=self.cfg.lr)
-        scheduler = lr_scheduler.CosineAnnealingLR(
-            optimizer, T_max=self.cfg.num_epochs, eta_min=0
+        base_optimizer = optim.SGD
+        optimizer = SAM(
+            self.model.parameters(),
+            base_optimizer,
+            lr=self.cfg.lr,
+            momentum=self.cfg.momentum,
+            weight_decay=self.cfg.weight_decay,
+            rho=self.cfg.rho,
         )
-        return {"optimizer": optimizer, "lr_scheduler": scheduler}
+
+        lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(
+            optimizer, T_max=self.cfg.num_epochs
+        )
+
+        return {"optimizer": optimizer, "lr_scheduler": lr_scheduler}
